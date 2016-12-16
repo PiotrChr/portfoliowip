f723a583558d2a6e9391112fd0ce0d5b4da35c3f
Piotr Chrusciel <p.chrusciel@7nxt.com>
2016-12-17 00:14:44 +0100

    Add minify css to gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var cleanCss = require('gulp-clean-css');
 
 gulp.task('css', function() {
 	return gulp.src('./css/scss/*.scss')
 			.pipe(sass().on('error', sass.logError))
+			.pipe(cleanCss({compatibility: 'ie9'}))
 			.pipe(gulp.dest('./css/web'))
 });
 
