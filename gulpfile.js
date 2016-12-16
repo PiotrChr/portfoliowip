04f7f7fd329f1da45925f952e65e5d055eb9360b
Piotr Chrusciel <p.chrusciel@7nxt.com>
2016-12-16 23:05:53 +0100

    Add gulpfile task for sass

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var sass = require('gulp-sass');
 gulp.task('css', function() {
 	return gulp.src('./css/scss/*.scss')
 			.pipe(sass().on('error', sass.logError))
-			.pipe(gulp.dest('./css'))
+			.pipe(gulp.dest('./css/web'))
 });
 
 gulp.task('sass:watch', function () {
