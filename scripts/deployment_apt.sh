0c1e3e70edc920e39972e0bcc1a3421989887b6b
Piotr Chrusciel <p.chrusciel@7nxt.com>
2016-12-16 23:54:07 +0100

    Deployment patch

--- scripts/deployment_apt.sh
+++ scripts/deployment_apt.sh
@@ -2,13 +2,18 @@
 syncFolder="/var/www/html/portfolio"
 . $syncFolder'/scripts/config.sh'
 
-# NPM
+# NODE & NPM
 msg "Installing Node"
 
 nodePath='/usr/bin/node'
 if [ ! -d "$nodePath" ]; then
     sudo apt-get -y install nodejs
     sudo ln -s `which nodejs` "$nodePath"
-    sudo npm install npm -g
 fi
 
+npmPath='/usr/bin/npm'
+if [ ! -d "$npmPath" ]; then
+    sudo apt-get -y install npm
+    sudo ln -s `which npm` "$npmPath"
+    sudo npm install npm -g
+fi
