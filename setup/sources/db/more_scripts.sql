443e900f9aec57a103b75e4ca0b1b0b24a88bc22
Piotr Chrusciel <p.chrusciel@7nxt.com>
2016-12-16 18:45:03 +0100

    Fix typo in more_scripts.sql

--- setup/sources/db/more_scripts.sql
+++ setup/sources/db/more_scripts.sql
@@ -1,4 +1,4 @@
-INSERT INTO `scripts` (title`, `subtitle`, `techniques`, `thumbnail`, `description_en`, `description_de`, `description_pl`, `link`) VALUES
+INSERT INTO `scripts` (`title`, `subtitle`, `techniques`, `thumbnail`, `description_en`, `description_de`, `description_pl`, `link`) VALUES
 ('New script', 'script subtitle', 'php,js,mysql', 'thumbnail.png', 'description in en', 'description in de', 'description in pl', 'www.google.pl'),
 ('New script', 'script subtitle', 'php,js,mysql', 'thumbnail.png', 'description in en', 'description in de', 'description in pl', 'www.google.pl'),
 ('New script', 'script subtitle', 'php,js,mysql', 'thumbnail.png', 'description in en', 'description in de', 'description in pl', 'www.google.pl'),
