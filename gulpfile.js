ac2ecc14e7098fea5299366a287aab8de081deb1
Piotr Chrusciel <p.chrusciel@7nxt.com>
2016-12-16 22:53:59 +0100

    Add gulp

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('css', function() {
+	return gulp.src('./css/scss/*.scss')
+			.pipe(sass().on('error', sass.logError))
+			.pipe(gulp.dest('./css'))
+});
+
+gulp.task('sass:watch', function () {
+	gulp.watch('./css/scss/*.scss', ['sass']);
+});
+
+// The default task (called when you run `gulp` from cli)
+gulp.task('default', ['sass']);
